fedff21198139fe0c47b50c1911dde28a57dc723
sin_iti <shinichilyers@gmail.com>
2017-12-15 16:02:03 +0800

    uncomplete

---
+++ class/st_devtool.js
@@ -0,0 +1,99 @@
+class StCli {
+    constructor() {
+        this.fontColors = {
+            "black": 30,
+            "red": 31,
+            "green": 32,
+            yellow: 33,
+            blue: 34,
+            purple: 35,
+            darkgreen: 36,
+			white: 37,
+			default: 39,
+        };
+        this.backgroundColors = {
+            black: 40,
+            red: 41,
+            green: 42,
+            yellow: 43,
+            blue: 44,
+            purple: 45,
+            darkgreen: 46,
+			white: 47,
+			default: 49
+        };
+        this.quotes = {
+            closeAllProperties: 0, //关闭所有属性
+            hightLight: 1, //设置高亮度
+            underline: 4, //下划线
+            twink: 5, //闪烁  
+            invert:7, //反显
+			fade: 8, //消隐,
+			//\x1b[30m   --  \x1b[37m   设置前景色  ,
+			//\x1b[40m   --  \x1b[47m   设置背景色
+			/*
+			*\x1b[nA    光标上移n行  
+			*\x1b[nB    光标下移n行  
+			*\x1b[nC    光标右移n行  
+			*\x1b[nD    光标左移n行  
+			*\x1b[y;xH  设置光标位置  
+			*\x1b[2J    清屏  
+			*\x1b[K     清除从光标到行尾的内容  
+			*\x1b[s     保存光标位置  
+			*\x1b[u     恢复光标位置  
+			*\x1b[?25l  隐藏光标  
+			*\x1b[?25h  显示光标
+			*/ 
+        };
+    }
+}
+
+
+class StEcho extends StCli {
+    constructor() {
+        super();
+    }
+    debug(str) {
+        this.log(str);
+        debugger;
+	}
+	getColorValue(colorStr, type = 'fontColors') {
+		var colors = this;
+		if (!colorStr) {
+			colorStr = colors[type].default;
+		} else {
+			if (colorStr in colors.fontColors) {
+				colorStr = colors[type][colorStr];
+			} else {
+				throw new Error(`${fontColor}不存在`);
+			}
+		}
+		return colorStr;
+	}
+    log(str, fontColor, backgroundColor) {
+		var type = '%s',
+			color,
+			colors = this;
+		if (typeof str == 'object') {
+			type = '%O';
+		}
+		fontColor = this.getColorValue(fontColor);
+		backgroundColor = this.getColorValue(backgroundColor, 'backgroundColors');
+		color = `\x1b[${backgroundColor};${fontColor}m${type}\x1b[${this.backgroundColors.default};${this.fontColors.default}m`;
+		console.log(color, str);
+	}
+	error(str) {
+		this.log(str, 'red', "black");
+	}
+	info(str){
+		this.log(str, 'yellow', 'black');
+	} 
+}
+
+
+let stEcho = new StEcho();
+
+module.exports = {
+	StEcho: StEcho,
+	stEcho: stEcho
+}
---
+++ class/st_string.js
@@ -0,0 +1,97 @@
+let {stEcho} = require("./st_devtool");
+
+class StString extends String{
+    constructor(str){
+        super(str);
+    };
+    toUnicode(str) {
+        if (!str) {
+            str = this.string;
+        }
+        let length = str.length,
+            codes = [];
+        for (let i = 0; i < length; i ++) {
+            codes[i] = '\\u' + str.charCodeAt(i).toString(16);
+        }
+        return codes.join('');
+    }
+}
+class StNum {
+    constructor() {
+        this.levels = ['', '十', '百', '千'];
+        this.level = this.levels.length;
+        this.layers = ['', '万', '亿'];
+    }
+    getSinoCode(num){
+        num = parseInt(num);
+        let sinoCodes = ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九'];
+        return sinoCodes[num];
+    }
+    sinicize(num, wholeUnit = '') {
+        let levels = this.levels,
+            layers = this.layers,
+            numArr = num.toString().split(''),
+            subIndex = numArr.length,
+            numObj = this,
+            levelLen = this.level,
+            numStr = '',
+            zeroUnicode = '零'.charCodeAt(0).toString(16),
+            zeroRE = new RegExp(`(\\u${zeroUnicode})+`, 'g');
+        numArr.reverse();
+        while(subIndex --) {
+            let levelIndex = subIndex % levelLen,
+                level = levels[levelIndex],
+                layerLevel = layers.length,
+                curNum = numArr[subIndex];
+            curNum = numObj.getSinoCode(curNum);
+            if (levelIndex == 0) {
+                let layerIndex = Math.floor(subIndex / levelLen);
+                layerIndex = layerIndex % layerLevel + Math.floor(layerIndex / layerLevel);
+                level += layers[layerIndex];
+                
+            } else if (curNum == '零') {
+                level = '';
+            }
+            if(curNum == '零') {
+                curNum = '';
+            }
+            numStr +=  curNum + level;             
+        }
+        numStr = numStr.replace(zeroRE, '零');
+        numStr += wholeUnit;
+        stEcho.log(numStr);
+        return numStr;
+    }
+}
+
+
+class StTimer extends StNum{
+    constructor (now) {
+        super();
+        if (!now) {
+            this.now = new Date();
+        } else {
+            if (typeof now == 'object') {
+                this.now = now;
+            } else {
+                this.now = new Date(now);
+            }
+        }
+        this.timestamp = this.now.getTime();
+        this.time = this.timestamp;
+    }
+    update(){
+        let timer = this;
+        timer = new StTimer();
+        return timer;
+    }
+    getMonth(sino = true) {
+        
+    } 
+}
+let myTimer = new StTimer();
+myTimer.sinicize(1002003);
+module.exports = {
+    StTimer:StTimer,
+    StNum: StNum
+}

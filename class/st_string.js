c09af050e884ae1d883d4a6f0bb794f418815a08
sin_iti <shinichilyers@gmail.com>
2017-12-15 18:56:11 +0800

    string

--- class/st_string.js
+++ class/st_string.js
@@ -19,47 +19,56 @@ class StString extends String{
 class StNum {
     constructor() {
         this.levels = ['', '十', '百', '千'];
-        this.level = this.levels.length;
         this.layers = ['', '万', '亿'];
     }
     getSinoCode(num){
         num = parseInt(num);
-        let sinoCodes = ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九'];
+        let sinoCodes = ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九', '十', '十一', '十二'];
         return sinoCodes[num];
     }
-    sinicize(num, wholeUnit = '') {
-        let levels = this.levels,
-            layers = this.layers,
-            numArr = num.toString().split(''),
-            subIndex = numArr.length,
-            numObj = this,
-            levelLen = this.level,
-            numStr = '',
-            zeroUnicode = '零'.charCodeAt(0).toString(16),
-            zeroRE = new RegExp(`(\\u${zeroUnicode})+`, 'g');
-        numArr.reverse();
-        while(subIndex --) {
-            let levelIndex = subIndex % levelLen,
-                level = levels[levelIndex],
-                layerLevel = layers.length,
-                curNum = numArr[subIndex];
-            curNum = numObj.getSinoCode(curNum);
-            if (levelIndex == 0) {
-                let layerIndex = Math.floor(subIndex / levelLen);
-                layerIndex = layerIndex % layerLevel + Math.floor(layerIndex / layerLevel);
-                level += layers[layerIndex];                
-            } else if (curNum == '零') {
-                level = '';
-            }
-            if(curNum == '零') {
-                curNum = '';
-            }
-            numStr +=  curNum + level;             
+    read(num) {
+        num = parseInt(num).toString().split('');
+        let sinoNumArr = [];
+        for (let n = 0, numLen = num.length; n < numLen; n++) {
+            sinoNumArr[n] = this.getSinoCode(num[n]);
         }
-        numStr = numStr.replace(zeroRE, '零');
-        numStr += wholeUnit;
-        stEcho.log(numStr);
-        return numStr;
+        return sinoNumArr.join('');
+    }
+    sinicize(num, wholeUnit = '') {
+        num = parseInt(num).toString().split('');
+        num.reverse();
+        let numLen = num.length,
+            levelLen = this.levels.length,
+            layerLen = this.layers.length,
+            numLayerNum = Math.ceil(numLen / levelLen),
+            tmpArr = [],
+            numObj = this,
+            numStrArr = [],
+            numStr;
+        for (let layer = 0; layer < numLayerNum; layer ++) {
+            tmpArr[layer] = [];
+            for (let level = 0; level < levelLen; level ++) {
+                let subIndex = layer * levelLen + level;
+                tmpArr[layer][level] = '';
+                if (subIndex < numLen) {
+                    let curNum = num[subIndex];
+                    if (curNum == 0) {
+                        if  (level == 1) {
+                            if (num[subIndex + 1] != 0) {
+                                tmpArr[layer][level] = '零';
+                            }
+                        } else if (level == 2) {
+                            tmpArr[layer][level] = '零';
+                        }
+                    } else {
+                        tmpArr[layer][level] = numObj.getSinoCode(curNum) + numObj.levels[level];
+                    }                    
+                }
+            }
+            numStrArr[layer] = tmpArr[layer].reverse().join('') +  this.layers[layer % layerLen];
+        }
+        numStr = numStrArr.reverse().join('');
+        return numStr + wholeUnit;
     }
 }
 
@@ -84,12 +93,115 @@ class StTimer extends StNum{
         timer = new StTimer();
         return timer;
     }
-    getMonth(sino = true) {
-        
-    } 
+    formatNum(num, pad = true, sinoMethod = false) {
+        if (pad) {
+            num = num.toString().padStart(2, '0');
+        }
+        if (sinoMethod) {
+            num = this[sinoMethod](num);
+        }
+        return num;
+    }
+    getYear(ellipsis = false, sino = false) {
+        let year = this.now.getFullYear();
+        if (ellipsis) {
+            year = year.toString().slice(2);
+        }
+        if (sino) {
+            year = this.read(year);
+        }
+        return year;
+    }
+    getMonth(pad = true,sino = false) {
+        let month = this.now.getMonth() + 1;
+        if (sino) {
+            sino = 'getSinoCode';
+        }
+        month = this.formatNum(month, pad, sino);
+        return month;
+    }
+    getDate(pad = true,sino = false) {
+        let date = this.now.getDate();
+        if (sino) {
+            sino = 'getSinoCode';
+        }
+        date = this.formatNum(date, pad, sino);    
+        return date;
+    }
+    getWeekDay(sino = false) {
+        let day = this.now.getDay();
+        if (sino) {
+            day = this.read(day);
+        }
+        return day;
+    }
+    getHour(pad = true, circle = 24, sino = false) {
+        let hour = this.now.getHours();
+        if (circle == 12 && hour > 12) {
+            hour = hour - 12;           
+        }  
+        if (sino) {
+            sino = 'sinicize';
+        }
+        hour = this.formatNum(hour, pad, sino);
+        return hour;
+    }
+    getMinute(pad = true, sino = false) {
+        let minute = this.now.getMinutes();
+        if (sino) {
+            sino = 'sinicize';
+        }
+        minute = this.formatNum(minute, pad, sino);
+        return minute;
+    }
+    getSecond(pad = true, sino = false) {
+        let second = this.now.getSeconds();
+        if (sino) {
+            sino = 'sinicize';
+        }
+        second = this.formatNum(second, pad, sino);
+        return second;
+    }
+    getMillisecond(pad = true, sino = false) {
+        let millisecond = this.now.getMilliseconds();
+        if (sino) {
+            sino = 'sinicize';
+        }
+        millisecond = this.formatNum(millisecond, pad, sino);
+        return millisecond;
+    }
+    format(format = "Y-m-d h:i:s", time = false, pad = true, sino = false) {
+        if (!time) {
+            time = this;
+        } else {
+            time = new StTimer(time);
+        }
+        let symbols = {
+            y: time.getYear(true, sino),
+            Y: time.getYear(false, sino),
+            m: time.getMonth(pad, sino),
+            d: time.getDate(pad, sino),
+            D: time.getWeekDay(sino),
+            h: time.getHour(pad, sino),
+            i: time.getMinute(pad, sino),
+            s:time.getSecond(pad, sino),
+            a: time.getMillisecond(pad, sino)
+        };
+        for (let theSymbol in symbols) {
+            let reg = `(\\W|^)${theSymbol}(\\W|$)`,
+                tag = 'i';
+            if (theSymbol.toLowerCase() == 'y' || theSymbol.toLowerCase() == 'd') {
+                tag = '';
+            }
+            reg = new RegExp(reg, tag);
+            format = format.replace(reg, "$1" + symbols[theSymbol] + "$2");
+        }
+        return format;
+    }
 }
+
 let myTimer = new StTimer();
-myTimer.sinicize(1002003);
+myTimer.format();
 module.exports = {
     StTimer:StTimer,
     StNum: StNum
